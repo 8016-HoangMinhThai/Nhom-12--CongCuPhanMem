4621f3e05d3a321355f2c3590a98fdfb5fe8e950
8016-HoangMinhThai <hoangminhthai27112003@gmail.com>
2025-04-08 11:23:42 +0700

    push full lai code

---
+++ thietbidientu/components/cuối_trang_user.php
@@ -0,0 +1,40 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>đường dẫn </h3>
+         <a href="trang_chủ.php"> <i class="fas fa-angle-right"></i><i class="fa-solid fa-house"></i>trang chủ</a>
+         <a href="giới_thiệu.php"> <i class="fas fa-angle-right"></i><i class="fa-solid fa-circle-info"></i>about</a>
+         <a href="trang_chủ.php?#cuahang"> <i class="fas fa-angle-right"></i><i class="fa-solid fa-shop"></i>cửa hàng</a>
+         <a href="liên_hệ.php"> <i class="fas fa-angle-right"></i><i class="fa-solid fa-address-book"></i>liên hệ</a>
+      </div>
+
+      <div class="box">
+         <h3>Liên kết phụ</h3>
+         <a href="login.php"> <i class="fas fa-angle-right"></i><i class="fa-solid fa-right-to-bracket"></i>đăng nhập</a>
+         <a href="register.php"> <i class="fas fa-angle-right"></i><i class="fa-regular fa-registered"></i>đăng ký</a>
+         <a href="giỏ_hàng.php"> <i class="fas fa-angle-right"></i><i class="fa-solid fa-cart-shopping"></i>giỏ hàng </a>
+         <a href="đơn_hàng.php"> <i class="fas fa-angle-right"></i><i class="fa-solid fa-truck-fast"></i>đơn hàng</a>
+      </div>
+
+      
+
+      <div class="box">
+         <h3>theo dõi chúng tôi</h3>
+         <a href="https://www.facebook.com/"><i class="fab fa-facebook-f"></i>facebook</a>
+         <a href="https://twitter.com/"><i class="fab fa-twitter"></i>twitter</a>
+         <a href="https://www.instagram.com/"><i class="fab fa-instagram"></i>instagram</a>
+         <a href="https://www.youtube.com/"><i class="fab fa-youtube"></i>youtobe</a>
+      </div>
+
+   </section>
+   <style>
+      .fa-star{
+         color: #f39c12;
+      }
+   </style>
+   
+   
+
+</footer>
